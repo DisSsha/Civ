f294b276784965fc89d31c8fc31766067b93bd6c
DisSsha <julien.merlin@gmail.com>
2019-05-04 17:40:51 +0200

    test error pdo

--- app/core/Cell.php
+++ app/core/Cell.php
@@ -58,7 +58,11 @@ class Cell {
 		if ($this->feature != null){
 			$feature = $this->feature->name;
 		}
-		$pdo->query("INSERT INTO `cells` (`id`, `game_id`,`turn`, `x`, `y`, `terrain`, `feature`, `bonus`) VALUES ('".$this->id."', '".$worldId."','".$turn."', '".$this->x."', '".$this->y."', '".$this->terrain->name."', '".$feature."', 'BONUSTODO') ON DUPLICATE KEY UPDATE;");
+		try{
+				$pdo->query("INSERT INTO `cells` (`id`, `game_id`,`turn`, `x`, `y`, `terrain`, `feature`, `bonus`) VALUES ('".$this->id."', '".$worldId."','".$turn."', '".$this->x."', '".$this->y."', '".$this->terrain->name."', '".$feature."', 'BONUSTODO') ON DUPLICATE KEY UPDATE;");
+		}catch PDOException $Exception ) {
+			 throw new MyDatabaseException( $Exception->getMessage( ) , $Exception->getCode( ) );
+		}
 
 	}
 
