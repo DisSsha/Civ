5440b55036c3339548a5d05b8afe8886608a5b95
root <root@vps675968.ovh.net>
2019-05-06 14:10:30 +0200

    before composer integration

--- app/utils/Database.php
+++ app/utils/Database.php
@@ -3,29 +3,29 @@ class Database {
 
     public static $instance = null;
 
-    $host = '127.0.0.1';
-    $db   = 'civ';
-    $user = 'civ';
-    $pass = 'civ';
-    $charset = 'utf8';
-    $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-    $opt = [
-        \PDO::ATTR_ERRMODE            => \PDO::ERRMODE_EXCEPTION,
-        \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
-        \PDO::ATTR_EMULATE_PREPARES   => false,
-    ];
 
     public static function getInstance() {
-        if(!self::$instance) {
-            try {
-                self::$instance = new \PDO($dsn, $user, $pass, $opt);
-            }
-            catch (PDOException $e) {
-                errorHandler(0, $e->getMessage(), $e->getFile(), $e->getLine(), print_r($e, true), 'sql');
-                die('SQL Error, terminating script.');
-            }
+      $host = '127.0.0.1';
+      $db   = 'civ';
+      $user = 'civ';
+      $pass = 'civ';
+      $charset = 'utf8';
+      $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+      $opt = [
+          \PDO::ATTR_ERRMODE            => \PDO::ERRMODE_EXCEPTION,
+          \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
+          \PDO::ATTR_EMULATE_PREPARES   => false,
+      ];
+      if(!self::$instance) {
+        try {
+            self::$instance = new \PDO($dsn, $user, $pass, $opt);
         }
-        return self::$instance;
+        catch (PDOException $e) {
+            errorHandler(0, $e->getMessage(), $e->getFile(), $e->getLine(), print_r($e, true), 'sql');
+            die('SQL Error, terminating script.');
+        }
+      }
+      return self::$instance;
     }
 
 }
